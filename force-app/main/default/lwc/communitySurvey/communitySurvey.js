d437e88ea55771665e94090369c86a58a43f22e2
Stefan Serkin <stefanserkin@gmail.com>
2023-08-08 02:25:11 -0400

    Build 0.1.0-9

--- force-app/main/default/lwc/communitySurvey/communitySurvey.js
+++ force-app/main/default/lwc/communitySurvey/communitySurvey.js
@@ -5,47 +5,42 @@ import { getRecord } from 'lightning/uiRecordApi';
 import initializeSurvey from '@salesforce/apex/CommunitySurveyController.initializeSurvey';
 import submitSurvey from '@salesforce/apex/CommunitySurveyController.submitSurvey';
 
-import SURVEY_OBJECT from '@salesforce/schema/Survey__c';
-import SURVEY_PAGINATED_FIELD from '@salesforce/schema/Survey__c.Paginated__c';
-import SURVEY_MAX_QUESTIONS_FIELD from '@salesforce/schema/Survey__c.Max_Questions_Per_Page__c';
-import SURVEY_QUESTION_OBJECT from '@salesforce/schema/Survey_Question__c';
-import SURVEY_RESPONSE_OBJECT from '@salesforce/schema/Survey_Response__c';
-import RESPONSE_SURVEY_FIELD from '@salesforce/schema/Survey_Response__c.Survey__c';
-import RESPONSE_CONTACT_FIELD from '@salesforce/schema/Survey_Response__c.Contact__c';
 import SURVEY_ANSWER_OBJECT from '@salesforce/schema/Survey_Answer__c';
 import ANSWER_QUESTION_FIELD from '@salesforce/schema/Survey_Answer__c.Question__c';
 import ANSWER_ANSWER_FIELD from '@salesforce/schema/Survey_Answer__c.Answer__c';
 import ANSWER_SURVEY_QUESTION_FIELD from '@salesforce/schema/Survey_Answer__c.Survey_Question__c';
-import ANSWER_SURVEY_RESPONSE_FIELD from '@salesforce/schema/Survey_Answer__c.Survey_Response__c';
 import ANSWER_DATA_TYPE_FIELD from '@salesforce/schema/Survey_Answer__c.Data_Type__c';
-import QUESTION_HELP_TEXT_FIELD from '@salesforce/schema/Survey_Answer__c.Help_Text__c';
+import SURVEY_NAME_FIELD from '@salesforce/schema/Survey__c.Name';
+import SURVEY_PAGINATED_FIELD from '@salesforce/schema/Survey__c.Paginated__c';
+import SURVEY_MAX_QUESTIONS_FIELD from '@salesforce/schema/Survey__c.Max_Questions_Per_Page__c';
+import SURVEY_TY_MESSAGE_FIELD from '@salesforce/schema/Survey__c.Thank_You_Message__c';
 
 export default class CommunitySurvey extends NavigationMixin(LightningElement) {
     @api recordId;
     currentPageReference;
     error;
     isLoading = false;
+    isSubmitted = false;
 
     /*********************************
-     * Survey answer data
+     * Survey question data
      *********************************/
-    wiredAnswers = [];
-    answers;
+    survey;
+    wiredQuestions = [];
+    questions;
 
     /*********************************
      * Object fields
      *********************************/
     fields = {
-        surveyPaginated: SURVEY_PAGINATED_FIELD, 
-        surveyMaxQuestions: SURVEY_MAX_QUESTIONS_FIELD, 
-        responseSurvey: RESPONSE_SURVEY_FIELD,
-        responseContact: RESPONSE_CONTACT_FIELD,
         answerQuestion: ANSWER_QUESTION_FIELD,
-        answerAnswer: ANSWER_ANSWER_FIELD, 
+        answerAnswer: ANSWER_ANSWER_FIELD,
         answerSurveyQuestion: ANSWER_SURVEY_QUESTION_FIELD,
-        answerSurveyResponse: ANSWER_SURVEY_RESPONSE_FIELD,
-        answerDataType: ANSWER_DATA_TYPE_FIELD, 
-        questionHelpText: QUESTION_HELP_TEXT_FIELD
+        answerDataType: ANSWER_DATA_TYPE_FIELD,
+        surveyName: SURVEY_NAME_FIELD,
+        surveyPaginated: SURVEY_PAGINATED_FIELD,
+        surveyMaxQuestions: SURVEY_MAX_QUESTIONS_FIELD,
+        thankYouMessage: SURVEY_TY_MESSAGE_FIELD
     };
 
     /*********************************
@@ -54,14 +49,18 @@ export default class CommunitySurvey extends NavigationMixin(LightningElement) {
     isPaginated = false;
     maxQuestionsPerPage = 5;
     currentPage = 1;
-    currentPageAnswers;
+    currentPageQuestions;
 
     get totalPages() {
         let result = 0;
-        if (this.answers && this.answers.length > 0 && this.maxQuestionsPerPage) {
-            result = Math.ceil(this.answers.length / this.maxQuestionsPerPage);
+        if (this.questions && this.questions.length > 0 && this.maxQuestionsPerPage) {
+            result = Math.ceil(this.questions.length / this.maxQuestionsPerPage);
         }
         return result;
+    }
+
+    get isMultiplePages() {
+        return this.totalPages && this.totalPages.length > 1;
     }
 
     get noQuestionsMessage() {
@@ -105,7 +104,7 @@ export default class CommunitySurvey extends NavigationMixin(LightningElement) {
      */
     @wire(getRecord, { 
         recordId : '$recordId', 
-        fields : [SURVEY_PAGINATED_FIELD, SURVEY_MAX_QUESTIONS_FIELD]
+        fields : [SURVEY_NAME_FIELD, SURVEY_PAGINATED_FIELD, SURVEY_MAX_QUESTIONS_FIELD, SURVEY_TY_MESSAGE_FIELD]
     }) wireSurvey({
         error,
         data
@@ -113,67 +112,51 @@ export default class CommunitySurvey extends NavigationMixin(LightningElement) {
         if (error) {
             this.error = error;
         } else if (data) {
-            // this.isPaginated = data.fields[this.fields.surveyPaginated.fieldApiName].value;
-            // this.maxQuestionsPerPage = data.fields[this.fields.surveyMaxQuestions.fieldApiName].value;
-            // console.log(':::: isPaginated: ', this.isPaginated );
-            // console.log(':::: isPaginated: ', this.maxQuestionsPerPage );
-            console.log('data --> ', data);
+            this.survey = data;
+            this.isPaginated = data.fields[this.fields.surveyPaginated.fieldApiName].value;
+            this.maxQuestionsPerPage = data.fields[this.fields.surveyMaxQuestions.fieldApiName].value;
         }
     }
 
+    get surveyTitle() {
+        return this.survey 
+            ? this.survey.fields[this.fields.surveyName.fieldApiName].value 
+            : 'Survey';
+    }
+
+    get thankYouMessage() {
+        return this.survey && this.survey.fields[this.fields.thankYouMessage.fieldApiName] != null
+            ? this.survey.fields[this.fields.thankYouMessage.fieldApiName].value
+            : 'Thank you!'
+    }
+
     /**
-     * Get collection of unsaved answered question records
+     * Get collection of question records
      * @param {*} recordId - Survey record id from flexipage
      */
     @wire(initializeSurvey, {recordId: '$recordId'})
     wiredResult(result) {
         this.isLoading = true;
-        this.wiredAnswers = result;
+        this.wiredQuestions = result;
         if (result.data) {
             let rows = JSON.parse( JSON.stringify(result.data) );
 
             let i = 0;
-            rows.forEach(ans => {
-                ans.qId = ans[this.fields.answerSurveyQuestion.fieldApiName];
-                ans.question = ans[this.fields.answerQuestion.fieldApiName];
-                ans.answer = this.getInitialFieldValue(i);
-                ans.dataType = this.setDataType(ans[this.fields.answerDataType.fieldApiName]);
-                ans.helpText = ans[this.fields.questionHelpText.fieldApiName];
+            rows.forEach(q => {
+                q.answer = this.getInitialFieldValue(i);
                 i++;
             });
 
-            this.answers = rows;
+            this.questions = rows;
             this.updateCurrentPageQuestions();
             this.error = undefined;
             this.isLoading = false;
         } else if (result.error) {
             console.error(result.error);
-            this.answers = undefined;
+            this.questions = undefined;
             this.error = result.error;
             this.isLoading = false;
         }
-    }
-
-    /**
-     * Set data type for each answer. Maps picklist value to supported
-     * value for lightning-input's 'type' attribute
-     * @param obj answer 
-     * @return void
-     */
-    setDataType(fieldDataType) {
-        let dataType = 'Text';
-        switch (fieldDataType) {
-            case 'Text':
-                dataType = 'text'
-                break;
-            case 'Number':
-                dataType = 'number'
-                break;
-            case 'Date':
-                dataType = 'date'
-                break;
-        }
-        return dataType;
     }
 
     /*********************************
@@ -182,57 +165,74 @@ export default class CommunitySurvey extends NavigationMixin(LightningElement) {
 
     handleInputChange(event) {
         const questionId = event.target.dataset.qid;
-        const updatedAnswers = this.answers.map((answer) => {
-            if (answer[this.fields.answerSurveyQuestion.fieldApiName] === questionId) {
-                return { ...answer, answer: String( event.target.value ) };
+        const updatedQuestions = this.questions.map((q) => {
+            if (q.id === questionId) {
+                return { ...q, answer: String( event.target.value ) };
             }
-            return answer;
+            return q;
         });
-        this.answers = updatedAnswers;
+        this.questions = updatedQuestions;
     }
 
-
     /**
-     * Set data type for each answer. Maps picklist value to supported
-     * value for lightning-input's 'type' attribute
-     * @param obj answer 
-     * @return void
+     * Submit survey
+     * Validate input
+     * Create survey response and survey answers
      */
     handleSubmitSurvey() {
+        const allValid = [...this.template.querySelectorAll('lightning-input')]
+            .reduce((validSoFar, inputFields) => {
+                inputFields.reportValidity();
+                return validSoFar && inputFields.checkValidity();
+            }, true);
 
-        const records = this.answers.map((ans) => {
-            let answer = {};
-            answer.sobjectType = SURVEY_ANSWER_OBJECT.objectApiName;
-            answer[this.fields.answerSurveyQuestion.fieldApiName] = ans[this.fields.answerSurveyQuestion.fieldApiName];
-            answer[this.fields.answerQuestion.fieldApiName] = String( ans.question );
-            answer[this.fields.answerAnswer.fieldApiName] = String( ans.answer );
-            answer[this.fields.answerDataType.fieldApiName] = ans[this.fields.answerDataType.fieldApiName];
-            return answer;
-        });
-        
-        submitSurvey({ 
-            recordId: this.recordId, 
-            contactId: this.contactId, 
-            answers: records
-        }).then((result) => {
-            const surveyResponseId = result;
-            console.log('::::: surveyResponseId ', surveyResponseId);
-            const event = new ShowToastEvent({
-                title: 'Success',
-                message: 'Thank you for your submission, but it is lousy. We have thrown it in the trash.',
-                variant: 'success'
+        if (allValid) {
+            const records = this.questions.map((q) => {
+                let answer = {};
+                answer.sobjectType = SURVEY_ANSWER_OBJECT.objectApiName;
+                answer[this.fields.answerSurveyQuestion.fieldApiName] = q.id;
+                answer[this.fields.answerQuestion.fieldApiName] = q.question;
+                answer[this.fields.answerAnswer.fieldApiName] = String( q.answer );
+                answer[this.fields.answerDataType.fieldApiName] = q.dataType;
+                return answer;
             });
-            this.dispatchEvent(event);
-        }).catch((error) => {
-            this.error = error;
-            console.error(this.error);
-            const event = new ShowToastEvent({
-                title: 'Oh no. It has not done the thing',
-                message: 'This did not go well.',
-                variant: 'error'
+            
+            submitSurvey({ 
+                recordId: this.recordId, 
+                contactId: this.contactId, 
+                answers: records
+            }).then((result) => {
+                this.isSubmitted = true;
+                const event = new ShowToastEvent({
+                    title: 'Thank you!',
+                    message: 'Your response has been recorded',
+                    variant: 'success'
+                });
+                this.dispatchEvent(event);
+            }).catch((error) => {
+                this.error = error;
+                console.error(this.error);
+                const event = new ShowToastEvent({
+                    title: 'Your response could not be recorded',
+                    message: this.getErrorMessage(),
+                    variant: 'error'
+                });
+                this.dispatchEvent(event);
             });
-            this.dispatchEvent(event);
-        });
+        } else {
+            console.log('Input is not valid');
+        }
+
+    }
+
+    getErrorMessage() {
+        let message = 'Unknown error';
+        if (Array.isArray(this.error.body)) {
+            message = this.error.body.map(e => e.message).join(', ');
+        } else if (typeof this.error.body.message === 'string') {
+            message = this.error.body.message;
+        }
+        return message;
     }
 
     /*********************************
@@ -256,7 +256,7 @@ export default class CommunitySurvey extends NavigationMixin(LightningElement) {
     updateCurrentPageQuestions() {
         const startIndex = (this.currentPage - 1) * this.maxQuestionsPerPage;
         const endIndex = startIndex + this.maxQuestionsPerPage;
-        this.currentPageAnswers = this.answers.slice(startIndex, endIndex);
+        this.currentPageQuestions = this.questions.slice(startIndex, endIndex);
     }
 
     get disablePreviousButton() {
